ea4761b20cdc663d62a87855ba41cf792b0a50d6
SaulD1998 <sauldavid619@live.com.mx>
2020-06-15 15:13:58 -0500

    SUbir

--- ejercicios/Semana1/1.cpp
+++ ejercicios/Semana1/1.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int operacion(int a);
+int restriccion(int n);
 
 int main(){
 	int n;
 	cin>>n;
-
-if(n>0 && n<100000){	
-		cout<<n<<"->";
 	
-	while(n!=1)
-	n=operacion(n);
-	}
-	
-
+	restriccion(n);
 return 0;
 }
 
@@ -32,5 +27,14 @@ int operacion(int a){
 	return p;
 }
 
+int restriccion(int n){
+	
+	if(n>0 && n<100000){	
+		cout<<n<<"->";
+	
+	while(n!=1)
+	n=operacion(n);
+	}
+}
 
 
